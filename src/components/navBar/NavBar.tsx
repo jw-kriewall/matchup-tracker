abfd912607a822faf708085f82d742e77dfc2cb0
jw-kriewall <jwkriewall@gmail.com>
2023-12-18 14:36:29 -0500

    Routing added for table

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,44 +1,90 @@
-import * as React from "react";
+import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import { useAppSelector } from "../../hooks/hooks";
-import LogoutButton from "../login/LogoutButton";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import { useNavigate } from "react-router-dom";
 import LoginButton from "../login/LoginButton";
+import LogoutButton from "../login/LogoutButton";
+import { useAppSelector } from "../../hooks/hooks";
 
 export default function NavBar() {
-	const user = useAppSelector(state => state.userReducer.user);
-	
+	const user = useAppSelector((state) => state.userReducer.user);
+	const [drawerOpen, setDrawerOpen] = React.useState(false);
+	const navigate = useNavigate();
+
+	const toggleDrawer = (open: any) => (event: any) => {
+		if (
+			event.type === "keydown" &&
+			(event.key === "Tab" || event.key === "Shift")
+		) {
+			return;
+		}
+
+		setDrawerOpen(open);
+	};
+
+	const handleNavigation = (path: string) => {
+		navigate(path);
+		setDrawerOpen(false);
+	};
+
+	const list = () => (
+		<Box
+			sx={{ width: 250 }}
+			role="presentation"
+			onClick={toggleDrawer(false)}
+			onKeyDown={toggleDrawer(false)}
+		>
+			<List>
+				<ListItem disablePadding>
+					<ListItemButton onClick={() => handleNavigation("/")}>
+						<ListItemText primary="Home" />
+					</ListItemButton>
+				</ListItem>
+				<ListItem disablePadding>
+					<ListItemButton onClick={() => handleNavigation("/data")}>
+						<ListItemText primary="Matchup Table" />
+					</ListItemButton>
+				</ListItem>
+				{/* Add more ListItems here for additional pages */}
+			</List>
+		</Box>
+	);
+
 	return (
-		<>
-			<Box sx={{ flexGrow: 1 }}>
-				<AppBar position="static">
-					<Toolbar>
-						<IconButton
-							size="large"
-							edge="start"
-							color="inherit"
-							aria-label="menu"
-							sx={{ mr: 4 }}
-						>
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static">
+				<Toolbar>
+					<IconButton
+						size="large"
+						edge="start"
+						color="inherit"
+						aria-label="menu"
+						sx={{ mr: 4 }}
+						onClick={toggleDrawer(true)}
+					>
 						<MenuIcon />
-						{/* @TODO: MenuIcon needs to show dropdown with routing to other pages */}
+					</IconButton>
 
-						</IconButton>
-						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-							MatchupTracker v1.0
-						</Typography>
+					<Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+						{list()}
+					</Drawer>
 
-						<div>
-							{ !user ? <LoginButton/> : <LogoutButton/> }
-						</div>
-						
-					</Toolbar>
-				</AppBar>
-			</Box>
-		</>
+					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+						MatchupTracker v1.0 BETA
+					</Typography>
+
+					<div>{!user ? <LoginButton /> : <LogoutButton />}</div>
+				</Toolbar>
+			</AppBar>
+		</Box>
 	);
 }
---
+++ src/types/TableTypes.tsx
@@ -0,0 +1,7 @@
+export type MatchupRecord = {
+    [deck: string]: string;
+};
+
+export type TableData = {
+    [deck: string]: MatchupRecord;
+};
