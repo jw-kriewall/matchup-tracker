dc6913d50ef35a8848a9ddc1e9258a0c97f9a9f6
jw-kriewall <jwkriewall@gmail.com>
2023-12-18 13:29:47 -0500

    More robust data table added

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -24,19 +24,30 @@ type MatchupRecord = {
     "Bulbasaur": {
       "Pikachu": "2-1-0",
       "Charizard": "0-2-1",
-      "Bulbasaur": "1-1-0"
+      "Bulbasaur": "1-1-0",
+      "Wartortle": "4-23-3"
+    },
+    "Wartortle": {
+      "Venusaur": "3-0-0",
+      "Golbat": "2-5-2",
+      "Wartortle": "1-1",
+      "Bulbasaur": "23-4-3"
     }
-    // ... add all other matchups here
   };
 
-  const decks = Object.keys(tableData);
+    const allDecks = new Set<string>();
+    Object.keys(tableData).forEach(deck => {
+        allDecks.add(deck);
+        Object.keys(tableData[deck]).forEach(opponent => allDecks.add(opponent));
+    });
+    const decks = Array.from(allDecks);
 
   function calculateWinPercentage(record: string): string {
     const [wins, losses, ties] = record.split('-').map(Number);
     const totalMatches = wins + losses; // Include ties in the total matches
   
     if(wins + losses + ties === 0) {
-        return "0.0"
+        return "0.0*"
     }
     // If there are no matches, or there are only ties (no wins or losses), return 50%
     if (wins === 0 && losses === 0) {
@@ -61,21 +72,21 @@ type MatchupRecord = {
         </thead>
         <tbody>
           {decks.map((deckRow, index) => (
-            <tr key={deckRow}>
-              <th>{deckRow}</th>
-              {decks.map((deckCol) => {
-                const record = tableData[deckRow][deckCol] || "0-0-0";
-                const winPercentage = calculateWinPercentage(record);
-                return (
-                  <td key={deckCol} style={{
-                    backgroundColor: `hsl(${winPercentage}, 100%, 50%)`,
-                    textAlign: "center"
-                  }}>
-                    {`${winPercentage}%`}
-                  </td>
-                );
-              })}
-            </tr>
+             <tr key={deckRow}>
+             <th>{deckRow}</th>
+             {decks.map(deckCol => {
+               const record = tableData[deckRow] ? (tableData[deckRow][deckCol] || "0-0-0") : "0-0-0";
+               const winPercentage = calculateWinPercentage(record);
+               return (
+                 <td key={deckCol} style={{
+                   backgroundColor: `hsl(${winPercentage}, 100%, 50%)`,
+                   textAlign: "center"
+                 }}>
+                   {`${winPercentage}%`}
+                 </td>
+               );
+             })}
+           </tr>
           ))}
         </tbody>
       </table>
