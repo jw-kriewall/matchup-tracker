64ebc39cd89f70f87344e8eb9314c1655ea346fc
jwkriewallLW <john.kriewall@lightwellinc.com>
2023-12-26 10:50:48 -0500

    Removed login to try again

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from "./pages/homePage/HomePage";
 import TablePage from "./pages/tablePage/TablePage";
-import { useAppDispatch } from "./hooks/hooks";
-import { logoutAction } from "./actions/userActions";
 
 function App() {
-	const dispatch = useAppDispatch();
-
-	useEffect(() => {
-		const signOutTimeStr: string | null = localStorage.getItem('signOutTime');
-
-		if (signOutTimeStr) {
-			const signOutTime: number = parseInt(signOutTimeStr, 10);
-			const currentTime: number = new Date().getTime();
-			const timeRemaining: number = signOutTime - currentTime;
-
-			if (timeRemaining > 0) {
-				// Set a timeout to sign the user out after the remaining time
-				setTimeout(() => dispatch(logoutAction()), timeRemaining);
-			} else {
-				dispatch(logoutAction());
-				window.location.reload();
-			}
-		}
-	}, []);
-
 	return (
 		<>
 			<BrowserRouter>
