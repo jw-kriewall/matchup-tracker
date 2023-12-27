e77acc17bbae566ffbfbefefdde3cbd6984d25f2
jwkriewallLW <john.kriewall@lightwellinc.com>
2023-12-26 21:09:43 -0500

    react-spring added for count change

--- src/components/matchups/countMatchups/CountMatchups.tsx
+++ src/components/matchups/countMatchups/CountMatchups.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { CredentialResponse } from '@react-oauth/google';
 import { useAppDispatch, useAppSelector } from '../../../hooks/hooks';
 import { Matchup } from '../../../types/MatchupModels';
 import { getMatchups } from '../../../apiCalls/matchups/getMatchups';
 import './CountMatchups.css';
+import { useSpring, animated } from 'react-spring';
 
 interface CountComponentProps {
   selectedDecks: string[],
@@ -12,10 +13,13 @@ interface CountComponentProps {
 
 export default function CountMatchups({ selectedDecks, user }: CountComponentProps) {
   const [count, setCount] = useState<number>(0);
+  const previousCountRef = useRef<number>(0);
   const matchups: Matchup[] = useAppSelector((state) => state.matchupReducer.matchups);
+  const props = useSpring({ number: count, from: { number: previousCountRef.current } });
+
   const dispatch = useAppDispatch();
 
-  if(matchups.length === 0) {
+  if (matchups.length === 0) {
     dispatch(getMatchups(user));
   }
 
@@ -28,12 +32,27 @@ export default function CountMatchups({ selectedDecks, user }: CountComponentPro
       setCount(matchupCount);
     };
     countMatchups();
-  }, [selectedDecks, matchups]);
+    previousCountRef.current = count;
+  }, [selectedDecks, matchups, count]);
+
+  const renderCount = () => {
+    const previousCount = previousCountRef.current;
+    let className = '';
+    if (count > previousCount) {
+      className = 'increase-animation';
+    } else if (count < previousCount) {
+      className = 'decrease-animation';
+    }
+
+    return <div className={className}>{count}</div>;
+  };
 
   return (
     <div className="matchup-count-display">
       <span>Matchup Count</span>
-      <div>{count}</div>
+      <animated.div>
+        {props.number.to(n => n.toFixed(0))}
+      </animated.div>
     </div>
   );
 };
