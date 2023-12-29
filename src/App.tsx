003d4eea1b4c4efbe52d92b3f0f251c4d9b45a24
jw-kriewall <jwkriewall@gmail.com>
2023-12-29 10:50:16 -0500

    Updating to version 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homePage/HomePage";
 import TablePage from "./pages/tablePage/TablePage";
 import SessionManagement from "./components/sessionManagement/SessionManagement";
+import {
+	ThemeProvider,
+	createMuiTheme,
+	makeStyles,
+} from "@material-ui/core/styles";
+
+const theme = createMuiTheme();
+
+const useStyles = makeStyles((theme: any) => {
+	root: {
+		// some CSS that accesses the theme
+	}
+});
 
 function App() {
 	return (
 		<>
-			<SessionManagement>
-				<BrowserRouter>
-					<Routes>
-						<Route path="/" element={<HomePage />} />
-						<Route path="/data" element={<TablePage />} />
-					</Routes>
-				</BrowserRouter>
-			</SessionManagement>
+			<ThemeProvider theme={theme}>
+				<SessionManagement>
+					<BrowserRouter>
+						<Routes>
+							<Route path="/" element={<HomePage />} />
+							<Route path="/data" element={<TablePage />} />
+						</Routes>
+					</BrowserRouter>
+				</SessionManagement>
+			</ThemeProvider>
 		</>
 	);
 }
