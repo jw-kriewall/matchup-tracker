6f4f6e0f341a745d806299627714da63ea560a59
jw-kriewall <jwkriewall@gmail.com>
2024-03-05 13:31:41 -0500

    Pushing progress

--- src/components/changeFormat/ChangeFormatButton.tsx
+++ src/components/changeFormat/ChangeFormatButton.tsx
@@ -1,11 +1,34 @@
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
+import { Collapse, List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import React from "react";
 
 export function ChangeFormatButton() {
 
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
     
     return (
-        <>
-            "Hello!"
-        </>
+      <List
+      sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+    >
+      
+      <ListItemButton onClick={handleClick}>
+        
+        <ListItemText primary="Inbox" />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItemButton sx={{ pl: 4 }}>
+            <ListItemText primary="Starred" />
+          </ListItemButton>
+        </List>
+      </Collapse>
+    </List>
     );
 }
--- src/components/profileDropdown/ProfileDropdown.tsx
+++ src/components/profileDropdown/ProfileDropdown.tsx
@@ -1,4 +1,16 @@
-import { Avatar, Box, IconButton, Menu, MenuItem, Tooltip, Typography } from "@mui/material";
+import {
+	Avatar,
+	Box,
+	Collapse,
+	IconButton,
+	List,
+	ListItemButton,
+	ListItemText,
+	Menu,
+	MenuItem,
+	Tooltip,
+	Typography,
+} from "@mui/material";
 import { CredentialResponse } from "@react-oauth/google";
 import { useCookies } from "react-cookie";
 import LogoutButton from "../login/LogoutButton";
@@ -6,6 +18,7 @@ import jwt_decode from "jwt-decode";
 import { DecodedJwtToken } from "../../types/DecodedJwtToken";
 import React from "react";
 import { ChangeFormatButton } from "../changeFormat/ChangeFormatButton";
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
 
 export function ProfileDropdown() {
 	const [userCookies] = useCookies(["user"]);
@@ -19,54 +32,70 @@ export function ProfileDropdown() {
 		userPicture = decodedToken.picture;
 	}
 
+	const [open, setOpen] = React.useState(true);
+
 	const handleClick = () => {
-		console.log(userPicture);
+		setOpen(!open);
 	};
 
-    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-    // const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+	const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+		null
+	);
 
-    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElUser(event.currentTarget);
-      };
-    
-      const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-      };
+	const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorElUser(event.currentTarget);
+	};
+
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+		setOpen(false);
+	};
 
 	return (
-        <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Profile settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Profile Pic" src={userPicture} />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              
-              <MenuItem>
-                <ChangeFormatButton />
-              </MenuItem>
+		<Box sx={{ flexGrow: 0 }}>
+			<Tooltip title="Profile settings">
+				<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+					<Avatar alt="Profile Pic" src={userPicture} />
+				</IconButton>
+			</Tooltip>
+			<Menu
+				sx={{ mt: "45px" }}
+				id="menu-appbar"
+				anchorEl={anchorElUser}
+				anchorOrigin={{
+					vertical: "top",
+					horizontal: "right",
+				}}
+				keepMounted
+				transformOrigin={{
+					vertical: "top",
+					horizontal: "right",
+				}}
+				open={Boolean(anchorElUser)}
+				onClose={handleCloseUserMenu}
+			>
+				<List
+					sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+					component="nav"
+					aria-labelledby="nested-list-subheader"
+				>
+					<ListItemButton onClick={handleClick}>
+						<ListItemText primary="Inbox" />
+						{open ? <ExpandLess /> : <ExpandMore />}
+					</ListItemButton>
+					<Collapse in={open} timeout="auto" unmountOnExit>
+						<List component="div" disablePadding>
+							<ListItemButton sx={{ pl: 4 }}>
+								<ListItemText primary="Starred" />
+							</ListItemButton>
+						</List>
+					</Collapse>
+				</List>
 
-              <MenuItem>
-                <LogoutButton />
-              </MenuItem>
-
-            </Menu>
-          </Box>
+				<MenuItem>
+					<LogoutButton />
+				</MenuItem>
+			</Menu>
+		</Box>
 	);
 }
